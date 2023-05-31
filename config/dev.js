30c508439e0c1e0df29d07a5a1ee98d3f0b61f50
Krishan <79534931+Krishan-Luhach@users.noreply.github.com>
2023-05-31 09:44:37 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURL: "mongodb+srv://krishan:zvYVLxNa1x0Ezsjq@cluster0.yxvso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET: "effegtaoglalsn",
+    SENDGRID_API: "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",
+    EMAIL: "http://localhost:3000"
+}
